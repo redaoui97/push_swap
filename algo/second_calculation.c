3b2143bf940500a37c16a6d010f98f3df5df3800
Nabil Reda <redanabil14@gmail.com>
2022-04-13 14:57:11 +0000

    going forward, I still have to fix it a bit

--- algo/second_calculation.c
+++ algo/second_calculation.c
@@ -6,7 +6,7 @@
 /*   By: rnabil <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 02:31:54 by rnabil            #+#    #+#             */
-/*   Updated: 2022/04/12 15:43:19 by rnabil           ###   ########.fr       */
+/*   Updated: 2022/04/13 14:56:32 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,37 +54,54 @@ static int calculate_inferior (node *stack_a, int value)
 	 return (count);
 }*/
 
-static node	*get_next_number(int value, node *stack_a)
+static node	*get_next_number(int value, node **stack_a)
 {
 	node	*next;
+	node	*first_node;
+	int		next_value;
 
-	next = stack_a;
-	while (stack_a)
+	next = *stack_a;
+	first_node = *stack_a;
+	while (*stack_a)
 	{
-		if (stack_a->value > value && stack_a->value < next->value)
-			next = stack_a;
-		stack_a = stack_a->next;
+		if((*stack_a)->next == NULL)
+			next_value = first_node->value;
+		else
+			next_value = (*stack_a)->next->value;
+		if ((*stack_a)->value < value && next_value > value)
+		{
+			next = *stack_a;
+		}
+		*stack_a = (*stack_a)->next;
 	}
+	*stack_a = first_node;
 	return (next);
 }
 
 void second_calculation(node **stack_a, node **stack_b)
 {
-	node	*first_node;
+	node	*first_node_a;
+	node	*first_node_b;
 	node	*next;
 
-	first_node = *stack_b;
+	first_node_b = *stack_b;
+	first_node_a = *stack_a;
 	while (*stack_b)
 	{
 		//WAAAAAAAAAAAAAAA ADD += 
 		
-		next = get_next_number((*stack_b)->value, *stack_a);
-		if ((next->position + 1)/list_size(*stack_a) > 0.5)
-			(*stack_b)->moves_count = next->position;
+		next = get_next_number((*stack_b)->value, &*stack_a);
+		if ((next->position + 1)/list_size(first_node_a) >= 0.5)
+		{
+			(*stack_b)->moves_count = next->position + 2;
+			ft_printf("1\n");
+		}
 		else
-			(*stack_b)->moves_count = list_size(*stack_a) - next->position;
+		{
+			(*stack_b)->moves_count = list_size(*stack_a) - (next->position);
+			ft_printf("2\n");
+		}
 		*stack_b = (*stack_b)->next;
-		ft_printf("--------\n");
 	}
-	*stack_b = first_node;
+	*stack_b = first_node_b;
 }
