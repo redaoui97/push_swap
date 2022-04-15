66f4f3fa0814bd500bdbf39391c60513f966c9b5
Redaoui97 <26080106+redaoui97@users.noreply.github.com>
2022-04-15 12:56:32 +0000

    Update calculate_moves.c

--- algo/calculate_moves.c
+++ algo/calculate_moves.c
@@ -6,50 +6,22 @@
 /*   By: rnabil <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 18:40:46 by rnabil            #+#    #+#             */
-/*   Updated: 2022/04/04 15:26:50 by rnabil           ###   ########.fr       */
+/*   Updated: 2022/04/15 09:30:02 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/push_swap.h"
 
-static void	first_calculation(node **stack_b)
-{
-	int		count;
-	int		size;
-	node	*first_ptr;
-
-	count = 0;
-	size = list_size(*stack_b);
-	first_ptr = *stack_b;
-	while (count < (size / 2))
-	{
-		(*stack_b)->moves_count = count++;
-		*stack_b = (*stack_b)->next;
-	}
-	if (!(size % 2))
-	{
-		(*stack_b)->moves_count = count--;
-		*stack_b = (*stack_b)->next;
-	}
-	while (count > 0)
-	{
-		(*stack_b)->moves_count = count--;
-		*stack_b = (*stack_b)->next;
-	}
-	*stack_b = first_ptr;
-}
-
 /*int void	calc_moves_a(int	value, node **stack_a)
-{		
+{
+		
 }
-
-static void	second_calculation(node **stack_b, node **stack_b)
+static void	second_calculation(node **stack_a, node **stack_b)
 {
 	int		count;
 	int		size;
 	node	*first_ptr;
 	node	*first_ptr_a;
-
 	count = 0;
 	size = list_size(*stack_b);
 	first_ptr = *stack_b;
@@ -59,10 +31,10 @@ static void	second_calculation(node **stack_b, node **stack_b)
 		*stack_b = (*stack_b)->next;
 	}
 	*stack_b = first_ptr;
-}*/
-
+}
+*/
 void	calculate_moves(node **stack_a, node **stack_b)
 {
 	first_calculation(&*stack_b);
-//	second_calculation(&*stack_a, &*stack_b);
+	second_calculation(&*stack_a, &*stack_b);
 }
